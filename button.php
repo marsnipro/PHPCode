c090680b7c04d0633a3f0a2f49933e014569a8ff
marsnipro <marsnipro@gmail.com>
2017-04-07 17:20:11 +0900

    Update button.php

--- button.php
+++ button.php
@@ -30,7 +30,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
   Name: <input type="text" name="name" value="<?php echo $name;?>">
   <span class="error">* <?php echo $nameErr;?></span>
-  <br><br>  
+  <br><br> 
+  <input type="submit" name="submit" value="Submit">  
 </form>
  
 <?php
