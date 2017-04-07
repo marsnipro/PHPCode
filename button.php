578763af257c658d00c84634db3056123722410b
marsnipro <marsnipro@gmail.com>
2017-04-07 17:09:36 +0900

    Update button.php

--- button.php
+++ button.php
@@ -7,21 +7,28 @@
 </head>
 <body>  
 
+$nameErr = "";
+$name = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  if (empty($_POST["name"])) {
+    $nameErr = "Name is required";
+  } else {
+    $name = test_input($_POST["name"]);
+    // check if name only contains letters and whitespace
+    if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
+      $nameErr = "Only letters and white space allowed"; 
+    }
+  }  
+  
 <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
   <input type="submit" name="submit" value="Submit">  
 </form>
 
+ 
 <?php
 echo "<h2>Your Input:</h2>";
 echo $name;
-echo "<br>";
-echo $email;
-echo "<br>";
-echo $website;
-echo "<br>";
-echo $comment;
-echo "<br>";
-echo $gender;
 ?>
 
 </body>
