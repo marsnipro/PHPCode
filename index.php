84228077b734d43db8fd113933a5e1c7d6890ebc
marsnipro <marsnipro@gmail.com>
2017-04-07 09:52:28 +0900

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,11 @@
 <?php
 echo "My first PHP script!";
 ?>
+  
+<?php
+$cars=array("Volvo","BMW","Toyota");
+echo "I like " . $cars[0] . ", " . $cars[1] . " and " . $cars[2] . ".";
+?>
 
 </body>
 </html>
