256faca69c79238f9145b2173a29ce8f702989f8
marsnipro <marsnipro@gmail.com>
2017-04-07 09:57:35 +0900

    Update index.php

--- index.php
+++ index.php
@@ -5,12 +5,12 @@
 <h1>My first PHP page</h1>
   
 <?php
-echo "My first PHP script!";  .<br>
+echo "My first PHP script!<br>"; 
 ?>
   
 <?php
 $cars=array("Volvo","BMW","Toyota");
-echo "I like " . $cars[0] . ", " . $cars[1] . " and " . $cars[2] . "."; .<br>
+echo "I like " . $cars[0] . ", " . $cars[1] . " and " . $cars[2] . ".";
 ?>
 
 </body>
